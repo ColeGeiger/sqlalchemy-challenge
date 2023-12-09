2e9a4a84ea2bda8a5d174835de6b4189930f1424
ColeGeiger <49625195+ColeGeiger@users.noreply.github.com>
2023-12-09 11:15:15 -0700

    updated code 2

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,4 +1,12 @@
 # Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, timedelta
+
+from flask import Flask, jsonify
 
 
 
@@ -8,23 +16,66 @@
 
 
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement 
+Station = Base.classes.station 
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
+
+@app.route("/api/v1.0/names")
+
+
+def precipitation():
+    """Return the last 12 months of precipitation data."""
+    
+    # Calculate the date one year ago from the last date in the database
+    one_year_ago = session.query(func.max(Measurement.date)).scalar() - timedelta(days=365)
+
+    # Query the precipitation data for the last 12 months
+    precipitation_data = session.query(Measurement.date, Measurement.prcp)\
+        .filter(Measurement.date >= one_year_ago)\
+        .order_by(Measurement.date).all()
+
+    # Organize the data into a dictionary
+    precipitation_dict = {date: prcp for date, prcp in precipitation_data}
+
+    # Return the JSON representation of the dictionary
+    return jsonify(precipitation_dict)
+
+def stations():
+    station_data = session.query(Station.station).all()
+
+    # Extract the stations from the query result
+    stations_list = [station[0] for station in station_data]
+
+    # Return the JSON representation of the list of stations
+    return jsonify(stations_list)
+
+def tobs():
+    
